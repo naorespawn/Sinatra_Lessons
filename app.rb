12974111b301c43e812541c34dcb3f64b71e6251
Naoki Egawa <nao.respawn@gmail.com>
2019-08-12 10:20:55 +0900

    create views

--- app.rb
+++ app.rb
@@ -2,29 +2,9 @@ require "sinatra"
 require "sinatra/reloader"
 
 get "/" do
-    "hello world"
+    erb :index
 end
 
-#get "/hello/:name" do
-#     "hello #{params[:name]}"
-#end
-
-#get "/hello/:name" do |name|
-#    "hello #{name}"
-#end
-
-#get "/hello/:fname/:lname" do |f,l|
-#    "hello #{f} #{l}"
-#end
-
-#get "/hello/:fname/?:lname?" do |f,l|
-#    "hello #{f} #{l}"
-#end
-
-#get "/hello/*/*" do
-#    "hello #{params[:splat][0]} #{params[:splat][1]}"
-#end
-
-get %r{/users/([0-9]*)} do
-    "user id = #{params[:captures][0]}"
+get "/about" do
+    erb :about
 end
