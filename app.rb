0300b0626eaf6ef9d40e4d06b0ae2fb4ded9621f
Naoki Egawa <nao.respawn@gmail.com>
2019-08-12 10:11:34 +0900

    regular expression

--- app.rb
+++ app.rb
@@ -17,6 +17,14 @@ end
 #    "hello #{f} #{l}"
 #end
 
-get "/hello/:fname/?:lname?" do |f,l|
-    "hello #{f} #{l}"
+#get "/hello/:fname/?:lname?" do |f,l|
+#    "hello #{f} #{l}"
+#end
+
+#get "/hello/*/*" do
+#    "hello #{params[:splat][0]} #{params[:splat][1]}"
+#end
+
+get %r{/users/([0-9]*)} do
+    "user id = #{params[:captures][0]}"
 end
