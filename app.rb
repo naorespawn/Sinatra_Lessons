2ab11bff6d59642bb717eb05fd50bfa827016940
Naoki Egawa <nao.respawn@gmail.com>
2019-08-13 18:32:24 +0900

    start BBS

--- app.rb
+++ app.rb
@@ -2,11 +2,6 @@ require "sinatra"
 require "sinatra/reloader"
 
 get "/" do
-    @title = "My Site"
+    @title = "My BBS"
     erb :index
-end
-
-get "/about" do
-    @title = "About This Site"
-    erb :about
 end
