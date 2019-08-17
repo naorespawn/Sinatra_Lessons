0875271dccea7d8f135fbf1f46e40cd4aa764b65
Naoki Egawa <nao.respawn@gmail.com>
2019-08-17 12:39:11 +0900

    add validate

--- app.rb
+++ app.rb
@@ -1,7 +1,23 @@
 require "sinatra"
 require "sinatra/reloader"
+require "active_record"
+
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: "./bbs.db"
+)
+
+class Comment < ActiveRecord::Base
+    validates :body, presence: true
+end
 
 get "/" do
     @title = "My BBS"
+    @comments = Comment.all
     erb :index
+end
+
+post "/create" do
+    Comment.create(body: params[:body])
+    redirect to("/")
 end
